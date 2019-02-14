2a643c2ac1f53e452b1aff1cf33c7eee7d4ff512
mcgeeze <mcgeeze@wgc.school.nz>
2019-02-15 09:45:10 +1300

    Add label, text and grid

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,8 @@ root = tkinter.Tk()
 root.title("Hello World!!!")
 root.geometry("600x200")
 
+my_label = tkinter.Label(root)
+my_label.config(text="Hello!", fg="green")
+my_label.grid()
+
 root.mainloop()
