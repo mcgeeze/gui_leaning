b6663ae0beb6e4998f85d25e59b7019eb40da2de
mcgeeze <mcgeeze@wgc.school.nz>
2019-02-15 10:01:37 +1300

    Added more labels and uwu with colour and fonts

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,20 @@
 import tkinter
+
 root = tkinter.Tk()
 
 root.title("Hello World!!!")
 root.geometry("600x200")
 
 my_label = tkinter.Label(root)
-my_label.config(text="Hello!", fg="green")
+my_label.config(text="OwO?", fg="pink", bg="#C8A2C8", font=("Comic Sans MS", "50", "bold"))
 my_label.grid()
+
+my_label2 = tkinter.Label(root)
+my_label2.config(text="UwU?!", fg="light blue", bg="pink", font=("Comic Sans MS", "30", "italic"))
+my_label2.grid()
+
+my_label3 = tkinter.Label(root)
+my_label3.config(text="OwO & UwU!", fg="#C8A2C8", bg="light blue", font=("Comic Sans MS", "25", "bold"))
+my_label3.grid()
 
 root.mainloop()
